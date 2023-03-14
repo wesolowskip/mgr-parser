e260754c0340be281b0426d098bf530674057808
Paweł <wesolowski.p@protonmail.com>
2023-03-14 20:08:31 +0100

    Dask preprocessing

---
+++ meta_cudf/dask_integration.h
@@ -0,0 +1,14 @@
+#ifndef META_JSON_PARSER_DASK_INTEGRATION_H
+#define META_JSON_PARSER_DASK_INTEGRATION_H
+#include <sys/types.h>
+
+struct block_data
+{
+    ssize_t last_eol;
+    ssize_t num_newlines;
+    bool win_eol;
+};
+
+block_data preprocess_block(const char* filename, size_t offset, size_t size = 0, bool force_host_read = false);
+
+#endif //META_JSON_PARSER_DASK_INTEGRATION_H
